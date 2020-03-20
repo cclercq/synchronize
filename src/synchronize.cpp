c95d10db848817e4a9135ca044981aba124a6058
Clement Calmels <clement.calmels@free.fr>
2020-03-20 14:16:02 +0100

    first implementation the synchronization

--- src/synchronize.cpp
+++ src/synchronize.cpp
@@ -4,18 +4,19 @@
 #include <mutex>
 #include <condition_variable>
 #include <list>
+#include <sstream>
 
 #include <ffmpeg.hpp>
 
 struct queue {
-	queue(size_t max_capacity = 30) : max(max_capacity), closed(false) {}
+	queue(size_t max_capacity = 30) : max(max_capacity), closed(false), t0(AV_NOPTS_VALUE) {}
 
 	void enqueue(av::frame &f) {
 		std::lock_guard<std::mutex> l(m);
-#if 0
+#if 1
 		std::cerr << (void*)this << std::dec
 			  << ": queue size: " << filled.size()
-			  << " enqueue frame: " << f.f->pts
+			  << " enqueue frame: " << f.f->pts << " " << t0 + f.f->pts
 			  << std::endl;
 #endif
 		filled.push_back(f);
@@ -41,6 +42,22 @@ struct queue {
 		return f;
 	}
 
+	bool acquire(int64_t pts, av::frame &f) {
+		std::lock_guard<std::mutex> l(m);
+		bool ret = false;
+
+		pts -= t0;
+
+		while (!filled.empty() && pts >= filled.front().f->pts ) {
+			f = filled.front();
+			filled.pop_front();
+
+			ret = true;
+		}
+
+		return ret;
+	}
+
 	void close(bool clear = false) {
 		std::lock_guard<std::mutex> l(m);
 
@@ -62,6 +79,7 @@ struct queue {
 	std::list<av::frame> filled;
 	size_t max;
 	bool closed;
+	int64_t t0;
 };
 
 static void read_input(av::input &in, queue &q)
@@ -100,10 +118,52 @@ static void read_input(av::input &in, queue &q)
 static void read_video(const std::string &url, queue &q)
 {
 	av::input in;
+	bool is_rtsp;
 
-	if (in.open(url))
+	is_rtsp = (url.compare(0,7, "rtsp://") == 0);
+
+	if (in.open(url, is_rtsp ? "rtsp_transport=tcp": "")) {
+		int64_t t0;
+
+		if (is_rtsp) {
+			// get t0 from start_real_time
+			av::packet p;
+
+			while (in >> p) {
+				int64_t realtime = in.start_time_realtime();
+
+				if (realtime != AV_NOPTS_VALUE) {
+					t0 = realtime;
+					break;
+				}
+			}
+		} else {
+			// get t0 from metadata
+			std::stringstream sstr(in.program_metadata(0));
+			std::string tmp;
+
+			while (getline(sstr, tmp, ':')) {
+				auto equal = tmp.find('=');
+
+				if (equal == std::string::npos)
+					continue;
+
+				if (tmp.compare(0, equal, "service_name") == 0) {
+					tmp.erase(0, equal + 1);
+
+					t0 = std::stoll(tmp);
+					break;
+				}
+			}
+		}
+
+		std::cerr << "t0: " << t0 << std::endl;
+
+		AVRational time_base = in.time_base(0);
+
+		q.t0 = av_rescale(t0, time_base.den, time_base.num * 1000000);
 		read_input(in, q);
-	else
+	} else
 		std::cerr << "Unable to open '" << url << "'" << std::endl;
 
 	q.close();
@@ -123,19 +183,25 @@ int main(int argc, char *argv[])
 	t1 = std::thread(read_video, argv[1], std::ref(q1));
 	t2 = std::thread(read_video, argv[2], std::ref(q2));
 
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
 	while (!q1.is_closed() && !q2.is_closed()) {
+
 		// start reading frame here
 		av::frame f1, f2;
 
 		f1 = q1.acquire();
 
-		std::cerr << "f1: " << f1.f->pts << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		std::cerr << "f1: " << q1.t0 + f1.f->pts << std::endl;
 
-		f2 = q2.acquire();
+//		f2 = q2.acquire();
 
-		std::cerr << "f2: " << f2.f->pts << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+//		std::cerr << "f2: " << q2.t0 + f2.f->pts << std::endl;
+
+		// now search the "good" pts in q2
+		if (q2.acquire(q1.t0 + f1.f->pts, f2)) {
+			std::cerr << "got f2: " << q2.t0 + f2.f->pts << std::endl;
+		}
 	}
 
 	t1.join();
